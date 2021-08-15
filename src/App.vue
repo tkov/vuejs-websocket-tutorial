bbd14fe10d69f236c344846a4ec0cea190e20142
Tim <tim.a.kovacs@gmail.com>
2021-08-15 18:45:45 -0400

    vuejs-websocket-tutorial

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,41 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <button v-on:click="sendMessage('Hello World!')">Send Message</button>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+
+  data: function() {
+    return {
+      connection: null
+    }
+  },
+  methods: {
+    sendMessage: function(message) {
+      console.log(this.connection);
+      this.connection.send(message);
+    }
+  },
+  created: function() {
+    console.log("Starting Connection to Web Server")
+    this.connection = new WebSocket("wss://echo.websocket.org")
+  
+    // callback function event handler for WebSocket.open
+    // (when a connection is opened...)
+    this.connection.onopen = function(event) {
+      console.log(event)
+      console.log("Successfully connected to the echo WebSocket Server")
+    }
+    // callback function event handler for WebSocket.message 
+    // (when a message is received from the server...)
+    this.connection.onmessage = function(event) {
+      console.log(event)
+    }
+  },
+
 }
 </script>
 
